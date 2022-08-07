944eb22401b6de2cacb328fcc7c0aea3312941ff
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-08-07 19:29:57 +0200

    heroe page

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { MaterialModule } from '../material/material.module';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
     HeroeComponent,
     HomeComponent,
     ListadoComponent,
-    HeroeTarjetaComponent
+    HeroeTarjetaComponent,
+    ImagenPipe
   ],
   imports: [
     CommonModule,
