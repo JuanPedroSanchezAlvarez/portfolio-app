333b1943068344bdcc526cc58e45d7ca88afe60c
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-08-13 13:36:34 +0200

    agregar y editar heroe

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -6,8 +6,14 @@ import { Heroe } from '../interfaces/heroes.interface';
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(heroe: Heroe): unknown {
-    return `assets/heroes/${heroe.id}.jpg`;
+  transform(heroe: Heroe): string {
+    if (!heroe.id && !heroe.alt_img) {
+      return 'assets/no-image.png';
+    } else if (heroe.alt_img) {
+      return heroe.alt_img;
+    } else {
+      return `assets/heroes/${heroe.id}.jpg`;
+    }
   }
 
 }
