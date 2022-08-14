cdf61210f154d35b98be74a21f5b39736848c51a
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-08-14 14:16:40 +0200

    Login y Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
@@ -9,7 +10,7 @@ const routes: Routes = [
   { path: 'ordenar', component: OrdenarComponent },
   { path: '**', redirectTo: '' }*/
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule) },
+  { path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule), canActivate: [AuthGuard], canLoad: [AuthGuard] },
   { path: '404', component: ErrorPageComponent },
   { path: '**', redirectTo: '404' }
 ];
---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanLoad {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.verificaAutenticacion().pipe(tap(estaAutenticado => {
+      if (!estaAutenticado) {
+        this.router.navigate(['./auth/login']);
+      }
+    }));
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.verificaAutenticacion().pipe(tap(estaAutenticado => {
+      if (!estaAutenticado) {
+        this.router.navigate(['./auth/login']);
+      }
+    }));
+  }
+
+}
