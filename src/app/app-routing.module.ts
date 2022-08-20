cd22494efc9b8bf72a51ae0bf74f7660b03b0715
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-08-20 17:17:29 +0200

    created components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  /*{ path: '', component: BasicosComponent, pathMatch: 'full' },
-  { path: 'numeros', component: NumerosComponent },
-  { path: 'no-comunes', component: NoComunesComponent },
-  { path: 'ordenar', component: OrdenarComponent },
-  { path: '**', redirectTo: '' }*/
+  { path: 'template', loadChildren: () => import('./template/template.module').then( m => m.TemplateModule) },
+  { path: 'reactive', loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule) },
+  { path: '**', redirectTo: 'template' }
 ];
 
 @NgModule({
