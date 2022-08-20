e2f38990d4bf3143f32baaf250964772da97329f
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-08-20 16:37:09 +0200

    reset project and get cdn bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth/guards/auth.guard';
-import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
   /*{ path: '', component: BasicosComponent, pathMatch: 'full' },
@@ -9,10 +7,6 @@ const routes: Routes = [
   { path: 'no-comunes', component: NoComunesComponent },
   { path: 'ordenar', component: OrdenarComponent },
   { path: '**', redirectTo: '' }*/
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule), canActivate: [AuthGuard], canLoad: [AuthGuard] },
-  { path: '404', component: ErrorPageComponent },
-  { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  baseUrl: 'http://ruta-backend/api'
+  production: true
 };
