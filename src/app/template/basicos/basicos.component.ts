0062006cc9fb909a39f6a6e2cd7778a83e0f7890
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-08-22 12:55:15 +0200

    template forms

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+  initForm = {
+    producto: 'RTX 4000',
+    precio: 10,
+    existencias: 10
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid && this.miFormulario?.controls['producto']?.touched;
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.touched && this.miFormulario?.controls['precio']?.value < 0;
+  }
+
+  guardar(): void {
+    console.log(this.miFormulario);
+    //this.miFormulario.resetForm();
+    this.miFormulario.resetForm({
+      producto: 'Sin nombre',
+      precio: 0,
+      existencias: 0
+    });
   }
 
 }
--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -7,9 +17,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  nuevoJuego: string = '';
+  persona: Persona = {
+    nombre: 'Fernando',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Metal Gear'
+      },
+      {
+        id: 2,
+        nombre: 'Death Stranding'
+      }
+    ]
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  agregarJuego(): void {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index: number): void {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  guardar(): void {
+    
   }
 
 }
