57c99d05b6b6a7e8c46957346f0df0859517e51b
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-11-07 13:56:23 +0100

    project reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { Pagina1Component } from './pages/pagina1/pagina1.component';
-import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Pagina1Component,
-    MuestraNombreComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
