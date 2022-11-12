1f7fd688c265f9b57e4f743ea670e040a746c2eb
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-11-12 10:48:10 +0100

    Install mapbox and implements first fullscreen map

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoibWlzaXNwaWNsaXgiLCJhIjoiY2xhY2p6eGRiMGJoMDNxcnRnMmM0dDh4YiJ9.RihH0MqKFTkfwkjo53E5bA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoibWlzaXNwaWNsaXgiLCJhIjoiY2xhY2p6eGRiMGJoMDNxcnRnMmM0dDh4YiJ9.RihH0MqKFTkfwkjo53E5bA'
 };
 
 /*
