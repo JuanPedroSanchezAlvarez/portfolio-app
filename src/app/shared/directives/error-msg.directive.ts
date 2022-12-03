1a4557c54825d107e4fa11202ac578c5b310a245
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-12-03 11:47:19 +0100

    Added functionality to directive

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,10 +1,34 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appErrorMsg]'
 })
-export class ErrorMsgDirective {
+export class ErrorMsgDirective implements OnInit {
 
-  constructor() { }
+  htmlElement: ElementRef<HTMLElement>;
+  @Input() color: string = 'red';
+  @Input() mensaje: string = 'Este campo es obligatorio.';
+
+  constructor(private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el;
+  }
+
+  ngOnInit(): void {
+    this.setColor();
+    this.setMensaje();
+    this.setBootstrapClass();
+  }
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this.color;
+  }
+
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this.mensaje;
+  }
+
+  setBootstrapClass(): void {
+    this.htmlElement.nativeElement.classList.add('form-text');
+  }
 
 }
