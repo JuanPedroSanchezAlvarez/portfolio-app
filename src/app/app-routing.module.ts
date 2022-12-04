e1cd23a820e44d209fd3952560a303328a8ffaed
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2022-12-04 13:23:25 +0100

    reset project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'productos', loadChildren: () => import('./productos/productos.module').then( m => m.ProductosModule ) },
-  { path: '**', redirectTo: 'productos' }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
